b4c30420c39b9f2ab1c84d8cc98c3e2ee2b6b6a5
heroboetc@mail.ru <gb00r1sb>
2021-04-25 16:17:41 +0300

    Collinear Points

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,25 +1,101 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 public class BruteCollinearPoints {
 
-    private LineSegment[] segments;
+    private final List<LineSegment> segments = new LinkedList<>();
 
     public BruteCollinearPoints(Point[] points) {
-        for (int i = 0; i < points.length - 3; i++) {
-            for (int j = 1; j < points.length - 2; j++) {
-
-                for (int k = 2; k < points.length - 1; k++) {
-                    for (int l = 3; l < points.length; l++) {
-
+        checkNullability(points);
+        Point[] copy = Arrays.copyOf(points, points.length);
+        Arrays.sort(copy);
+        checkSimilarPoints(copy);
+        for (int i = 0; i < copy.length - 3; i++) {
+            for (int j = i + 1; j < copy.length - 2; j++) {
+                for (int k = j + 1; k < copy.length - 1; k++) {
+                    for (int l = k + 1; l < copy.length; l++) {
+                        Point first = copy[i];
+                        Point second = copy[j];
+                        Point third = copy[k];
+                        Point fourth = copy[l];
+                        double targetSlope = first.slopeTo(second);
+                        if (first.slopeTo(third) == targetSlope && first.slopeTo(fourth) == targetSlope) {
+                            segments.add(new LineSegment(first, fourth));
+                        }
                     }
                 }
             }
         }
     }
 
+    private void checkNullability(Point[] points) {
+        checkArgument(points);
+        for (Point point : points) {
+            checkArgument(point);
+        }
+    }
+
+    private void checkSimilarPoints(Point[] points) {
+        Point previousPoint = null;
+        for (Point point : points) {
+            if (Objects.equals(previousPoint, point)) {
+                throw new IllegalArgumentException();
+            }
+            previousPoint = point;
+        }
+    }
+
+    private void checkArgument(Object o) {
+        if (o == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public int numberOfSegments() {
-        return segments.length;
+        return segments.size();
     }
 
     public LineSegment[] segments() {
-        return segments;
+        return segments.toArray(new LineSegment[0]);
+    }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        printPoints(points);
+    }
+
+    private static void printPoints(Point[] points) {
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 }
--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -2,19 +2,116 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+
 public class FastCollinearPoints {
 
-    public FastCollinearPoints(Point[] points) {
+    private final List<LineSegment> segments = new LinkedList<>();
 
-    }     // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+        checkNullability(points);
+        Point[] copy = Arrays.copyOf(points, points.length);
+        Arrays.sort(copy);
+        checkSimilarPoints(copy);
+        for (int i = 0; i < copy.length - 3; i++) {
+            Point point = copy[i];
+            Tuple[] pointsWithSlopes = getSortedPointsWithSlopes(copy, point, i + 1);
+            segments.addAll(getLineSegments(point, pointsWithSlopes));
+        }
+    }
+
+    private Tuple[] getSortedPointsWithSlopes(Point[] points, Point point, int indent) {
+        Tuple[] pointsWithSlopes = new Tuple[points.length - indent];
+        int pointer = 0;
+        for (int j = indent; j < points.length; j++) {
+            Point comparedPoint = points[j];
+            pointsWithSlopes[pointer++] = new Tuple(comparedPoint, point.slopeTo(comparedPoint));
+        }
+        Arrays.sort(pointsWithSlopes);
+        return pointsWithSlopes;
+    }
+
+    private List<LineSegment> getLineSegments(Point point, Tuple[] pointsWithSlopes) {
+        List<LineSegment> result = new ArrayList<>();
+        List<Point> alignedPoints = new ArrayList<>();
+        Tuple firstTuple = pointsWithSlopes[0];
+        alignedPoints.add(firstTuple.getPoint());
+        double currentSlope = firstTuple.getSlope();
+        for (int j = 1; j < pointsWithSlopes.length; j++) {
+            Tuple currentTuple = pointsWithSlopes[j];
+            if (currentSlope != currentTuple.getSlope()) {
+                if (alignedPoints.size() > 3) {
+                    result.add(new LineSegment(point, alignedPoints.get(alignedPoints.size() - 1)));
+                }
+                alignedPoints.clear();
+            }
+            alignedPoints.add(currentTuple.getPoint());
+            currentSlope = currentTuple.getSlope();
+        }
+        if (alignedPoints.size() > 3) {
+            result.add(new LineSegment(point, alignedPoints.get(alignedPoints.size() - 1)));
+        }
+        return result;
+    }
 
     public int numberOfSegments() {
-        return 0;
-    }        // the number of line segments
+        return segments.size();
+    }
 
     public LineSegment[] segments() {
-        return null;
-    }                // the line segments
+        return segments.toArray(new LineSegment[0]);
+    }
+
+    private void checkNullability(Point[] points) {
+        checkArgument(points);
+        for (Point point : points) {
+            checkArgument(point);
+        }
+    }
+
+    private void checkSimilarPoints(Point[] points) {
+        Point previousPoint = null;
+        for (Point point : points) {
+            if (Objects.equals(previousPoint, point)) {
+                throw new IllegalArgumentException();
+            }
+            previousPoint = point;
+        }
+    }
+
+    private void checkArgument(Object o) {
+        if (o == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static class Tuple implements Comparable<Tuple> {
+        private final Point point;
+        private final double slope;
+
+        Tuple(Point point, double slope) {
+            this.point = point;
+            this.slope = slope;
+        }
+
+        public Point getPoint() {
+            return point;
+        }
+
+        public double getSlope() {
+            return slope;
+        }
+
+        @Override
+        public int compareTo(Tuple o) {
+            return Double.compare(this.getSlope(), o.getSlope());
+        }
+    }
 
     public static void main(String[] args) {
 
